56f02f71a2b13adcd5784a5fe5b254b1a3e69bd1
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-25 11:42:11 +0530

    added custom fetch api

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
+import { getPosts } from '../api'
+
 function App() {
+  useEffect(() => {
+    async function fetchPost() {
+      const response = await getPosts()
+      console.log(response)
+    }
+    fetchPost()
+  }, [])
+
   return (
     <div className='App'>
       <h1>Codial app</h1>
