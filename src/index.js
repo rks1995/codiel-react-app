f6392a33f428b3644e7e53f06fec47fb400ee401
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-26 14:52:25 +0530

    added notications

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import { App } from './components'
 
+import { Toaster } from 'react-hot-toast'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,38 @@
 import styles from '../styles/login.module.css'
+import { useFormInput } from '../hooks'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const Login = () => {
+  const email = useFormInput('')
+  const password = useFormInput('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!email.value || !password.value) {
+      return toast.error('Invalid credential')
+    }
+    setIsLoggedIn(true)
+  }
+
   return (
-    <form action='' className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Login</span>
       <div className={styles.field}>
-        <input type='email' name='' placeholder='email' required />
+        <input type='email' name='email' {...email} placeholder='email' />
       </div>
       <div className={styles.field}>
-        <input type='password' name='' placeholder='password' required />
+        <input
+          type='password'
+          name='password'
+          {...password}
+          placeholder='password'
+        />
       </div>
       <div className={styles.field}>
-        <button>Login</button>
+        <button>{isLoggedIn ? 'Logging in...' : 'Log In'}</button>
       </div>
     </form>
   )
